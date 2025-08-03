579993bd3df5f1890861ea95351643223e617936
XavierLaforgue <xavierjimmys@gmail.com>
2025-08-03 17:37:18 +0200

    3-to_str.py creaet
    Create annotated functio that returns string representation of a float

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Contains annotated function that returns string representation."""
+
+
+def to_str(n: float) -> str:
+    """Return string representation of float."""
+    return str(n)
