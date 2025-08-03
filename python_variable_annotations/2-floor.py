548322c223bfdde0ef787f403cc630458d945d21
XavierLaforgue <xavierjimmys@gmail.com>
2025-08-03 17:14:55 +0200

    2-floor.py create
    Create annotated function that takes the floor of a float

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Contains annotated function that returns the floor of a float."""
+
+
+def floor(n: float) -> int:
+    """
+    Take a float and return its floor.
+
+    :param n: number
+    :type n: float
+    :return: floor of n
+    :rtype: int
+    """
+    return floor(n)
