4fafb17c77b1ee81f8a81bec8801459a7209f37c
XavierLaforgue <xavierjimmys@gmail.com>
2025-08-03 17:55:23 +0200

    6-sum_mixed_list.py create
    Create annotated function that adds elements in a mixed list of floats and ints

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Contains annotated function that resturns sum of mixed list as float."""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+    """Sum elements in list of floats and ints."""
+    sum: float = 0
+    for n in mxd_lst:
+        sum = sum + n
+    return sum
