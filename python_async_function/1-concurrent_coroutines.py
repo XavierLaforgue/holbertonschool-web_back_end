be11c2aaa5a8544a2d08aea08d416346a0cd2a0a
XavierLaforgue <xavierjimmys@gmail.com>
2025-08-10 23:55:15 +0200

    fix: update type annotations in wait_n function for consistency

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 """Contains function that awaits concurrently a given nuber of times."""
 import asyncio
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """Wait concurrently and return wait duration list in seconds."""
-    tasks: list[asyncio.Task]
+    tasks: List[asyncio.Task]
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
-    results: list[float] = []
+    results: List[float] = []
     completed: asyncio.Future[float]
     for completed in asyncio.as_completed(tasks):
         result: float = await completed
