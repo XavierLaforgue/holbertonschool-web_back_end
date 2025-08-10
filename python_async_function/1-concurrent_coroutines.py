354ec336bc42e8e62cc33810f712f21259062c55
XavierLaforgue <xavierjimmys@gmail.com>
2025-08-10 23:39:29 +0200

    fix: add type annotation for tasks in wait_n function

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -6,7 +6,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> list[float]:
     """Wait concurrently and return wait duration list in seconds."""
-    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    tasks: list[asyncio.Task] = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
     results: list[float] = []
     for completed in asyncio.as_completed(tasks):
         result = await completed
