a5723c22abae52d2755d0c7ffac13da44a7ec187
XavierLaforgue <xavierjimmys@gmail.com>
2025-08-11 12:59:27 +0200

    1-simple_pagination.py create
    Create class that emulates a server that sends back paginated content

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+"""Contains server emulator that sends pages according to set pagination."""
+
+import csv
+import math
+from typing import List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Return index range of current page.
+
+    :param page: current page number
+    :type page: int
+    :param page_size: used page size
+    :type page_size: int
+    :return: index range for the current page
+    :rtype: tuple[int, int]
+    """
+    start_idx: int = page_size * (page - 1)
+    end_idx: int = page_size * page
+    page_content: Tuple[int, int] = (start_idx, end_idx)
+    return page_content
+
+
+class Server:
+    """Server class to paginate a database of popular baby names."""
+
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        """Instanciate Server."""
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cache dataset."""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Get content that will make up the current page.
+
+        :param page: current page number
+        :type page: int
+        :param page_size: size of each page
+        :type page_size: int
+        :return: content of the page
+        :rtype: List[List[Any]]
+        """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+        list_bounds: Tuple[int, int] = index_range(page, page_size)
+        cached_data: List[List] = self.dataset()
+        return cached_data[list_bounds[0]:list_bounds[1]]
