25eedb334fea881c100b5566abe5004fa5c505e2
XavierLaforgue <xavierjimmys@gmail.com>
2025-08-12 17:39:45 +0200

    refactor: move MongoDB log stats functionality inside main guard

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3
 """Contains Python script providing stats on Nginx logs stored in MongoDb."""
-from pymongo import MongoClient
 
-client = MongoClient('mongodb://127.0.0.1:27017')
-nginx_collection = client.logs.nginx
-print(f'{nginx_collection.count_documents({})} logs')
-print('Methods:')
-print(f'    method GET: {nginx_collection.count_documents({"method": "GET"})}')
-print(f'    method POST: {nginx_collection.count_documents({"method": "POST"})}')
-print(f'    method PUT: {nginx_collection.count_documents({"method": "PUT"})}')
-docs_with_patch: int = nginx_collection.count_documents({"method": "PATCH"})
-print(f'    method PATCH: {docs_with_patch}')
-docs_with_delete: int = nginx_collection.count_documents({"method": "DELETE"})
-print(f'    method DELETE: {docs_with_delete}')
-docs_get_path: int = nginx_collection.count_documents(
-    {"method": "GET", "path": "/status"})
-print(f'{docs_get_path} status check')
+if __name__ == "__main__":
+    from pymongo import MongoClient
+
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
+    print(f'{nginx_collection.count_documents({})} logs')
+    print('Methods:')
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        num_docs: int = nginx_collection.count_documents({"method": method})
+        print(f'    method {method}: {num_docs}')
+    docs_get_path: int = nginx_collection.count_documents(
+        {"method": "GET", "path": "/status"})
+    print(f'{docs_get_path} status check')
