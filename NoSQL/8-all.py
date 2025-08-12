915914d972a3046fec1c5779339f27bbc43d2d92
XavierLaforgue <xavierjimmys@gmail.com>
2025-08-12 15:19:28 +0200

    fix: update docstring and import statements in 8-all.py for clarity

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
-"""
-Docstring for 8-all
-"""
-from pymongo.collection import Collection, Cursor
+"""Contains a function that retrieves a list of documents in a mongoDB."""
+from pymongo.collection import Collection
+from pymongo.cursor import Cursor
 
 
 def list_all(mongo_collection: Collection) -> list:
+    """
+    List all documents in given collection.
+
+    :param mongo_collection: name of the collection
+    :type mongo_collection: Collection
+    :return: list of the documents
+    :rtype: list[Any]
+    """
     pymongo_cursor: Cursor = mongo_collection.find()
     return list(pymongo_cursor)
