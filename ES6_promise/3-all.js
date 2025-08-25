8c7a18789bf7718c39d43fa4f1b2b89c7299506e
XavierLaforgue <xavierjimmys@gmail.com>
2025-08-25 17:33:53 +0200

    3-all.js create
    Create function that handles multiple functions that return promises

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,9 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+    .then(([res1, res2]) => {
+      console.log(`${res1.body} ${res2.firstName} ${res2.lastName}`);
+    })
+    .catch(() => { console.log('Signup system offline'); });
+}
