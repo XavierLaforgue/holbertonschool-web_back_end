890f0151e19a549ff4e4106400f8efc82355fd21
XavierLaforgue <xavierjimmys@gmail.com>
2025-08-25 15:06:29 +0200

    8-clean_set.js update
    Update function so it returns empty string when startString is empty or no given

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 export default function cleanSet(set, startString) {
   const leftoverStrArr = [];
   for (const str of set.values()) {
-    if (startString.length > 0 && str.startsWith(startString)) {
+    if (startString && startString.length > 0 && str.startsWith(startString)) {
       leftoverStrArr.push(str.slice(startString.length));
     }
   }
