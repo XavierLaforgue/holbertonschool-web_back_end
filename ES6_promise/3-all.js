341755cac4a4342e09a1ef724feb1ee7a5d36338
XavierLaforgue <xavierjimmys@gmail.com>
2025-08-25 17:34:39 +0200

    Fix handleProfileSignup to return the promise from Promise.all

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then(([res1, res2]) => {
       console.log(`${res1.body} ${res2.firstName} ${res2.lastName}`);
     })
