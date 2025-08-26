8c66c04ca267dc26d758f2f7d3a3b498811dce31
XavierLaforgue <xavierjimmys@gmail.com>
2025-08-26 11:33:53 +0200

    6-final-user.js update
    Update return value so it includes the error message as a string in the property value (same format as succesful promise)

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,8 +1,40 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+export default async function handleProfileSignup(
+  firstName, lastName, fileName,
+) {
   return Promise.allSettled(
     [signUpUser(firstName, lastName), uploadPhoto(fileName)],
-  );
+  ).then((results) => {
+    const resultsClean = results.map((result) => {
+      if (result.status === 'rejected') {
+        return {
+          status: result.status,
+          value: `Error: ${result.reason.message}`,
+        };
+      }
+      return {
+        status: result.status,
+        value: result.value,
+      };
+    });
+    return resultsClean;
+  });
 }
+
+// export default function handleProfileSignup(firstName, lastName, fileName) {
+//   return Promise.allSettled(
+//     [signUpUser(firstName, lastName), uploadPhoto(fileName)],
+//   ).then((results) => {
+//     results.map((result) => {
+//       if (result.status === 'rejected') {
+//         return {
+//           status: 'rejected',
+//           value: `Error: ${result.reason.message}`,
+//         };
+//       }
+//       return result;
+//     });
+//   });
+// }
