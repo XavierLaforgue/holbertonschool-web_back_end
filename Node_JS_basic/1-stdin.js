79ac89952ca49f82801f76e59a4c914a327c1695
XavierLaforgue <xavierjimmys@gmail.com>
2025-09-02 12:10:50 +0200

    1-stdin.js update
    Execute the script in a child process

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,14 +1,25 @@
 #!/usr/bin/node
+const { spawn } = require('node:child_process');
 const { createInterface } = require('node:readline');
 
-const readInput = createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-readInput.question('Welcome to Holberton School, what is your name?\n',
-  (answer) => {
+function askName() {
+  const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question('Welcome to Holberton School, what is your name?\n', (answer) => {
     console.log(`Your name is: ${answer}`);
-    readInput.close();
+    rl.close();
+  });
+}
+
+if (process.argv[2] === 'child') {
+  askName();
+} else {
+  const child = spawn(process.execPath, [__filename, 'child'], {
+    stdio: 'inherit',
+  });
+  child.on('exit', () => {
     console.log('This important software is now closing');
   });
+}
