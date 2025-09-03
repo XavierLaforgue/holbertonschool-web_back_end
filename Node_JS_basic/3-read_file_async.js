0bd7f599614db0150badbd7bc5758073e97092fb
XavierLaforgue <xavierjimmys@gmail.com>
2025-09-03 15:27:48 +0200

    3-read_file_async.js create
    Create countStudents function that reads the file asynchronously

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -1,41 +1,31 @@
-const fs = require('fs');
-const readline = require('node:readline');
+const fsPromises = require('node:fs/promises');
 
 function countStudents(aPath) {
-  let file;
   try {
-    file = readline.createInterface({
-      input: fs.createReadStream(aPath, { start: 1, encoding: 'utf8' }),
-      terminal: false,
+    const file = fsPromises.readFile(aPath, { encoding: 'utf8' });
+    file.then((fileResponse) => {
+      const linesArray = fileResponse.split('\n');
+      const studentsArray = linesArray
+        .slice(1, -1).filter((line) => line.trim().length !== 0)
+        .map((line) => line.split(','))
+        .map((student) => student.map((property) => property.trim()));
+      const studentsFields = new Map();
+      console.log(`Number of students: ${studentsArray.length}`);
+      for (const student of studentsArray) {
+        if (!studentsFields.has(student[3])) {
+          studentsFields.set(student[3], [student[0]]);
+        } else {
+          studentsFields.get(student[3]).push(student[0]);
+        }
+      }
+      for (const [field, students] of studentsFields.entries()) {
+        console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
+      }
     });
+    return file;
   } catch (err) {
     throw new Error('Cannot load the database');
   }
-  let isFirstLine = true;
-  let numberStudents = 0;
-  const studentsFields = new Map();
-  file.on('line', (line) => {
-    if (isFirstLine || line.trim() === '') {
-      isFirstLine = false;
-      return;
-    }
-    numberStudents += 1;
-    const student = line.split(',');
-    if (!studentsFields.has(student[3])) {
-      studentsFields.set(student[3], [student[0]]);
-    } else {
-      studentsFields.get(student[3]).push(student[0]);
-    }
-  });
-  file.on('error', () => {
-    throw new Error('Cannot load the database');
-  });
-  file.on('close', () => {
-    console.log(`Number of students: ${numberStudents}`);
-    for (const [field, students] of studentsFields.entries()) {
-      console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
-    }
-  });
 }
 
 module.exports = countStudents;
