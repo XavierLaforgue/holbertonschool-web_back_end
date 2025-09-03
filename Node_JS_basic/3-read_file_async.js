7bd36db1b6fc53a5172562b927ec8be0b6510c76
XavierLaforgue <xavierjimmys@gmail.com>
2025-09-03 15:35:36 +0200

    Refactor countStudents function to remove try-catch block and streamline file reading process

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -1,31 +1,29 @@
 const fsPromises = require('node:fs/promises');
 
 function countStudents(aPath) {
-  try {
-    const file = fsPromises.readFile(aPath, { encoding: 'utf8' });
-    file.then((fileResponse) => {
-      const linesArray = fileResponse.split('\n');
-      const studentsArray = linesArray
-        .slice(1, -1).filter((line) => line.trim().length !== 0)
-        .map((line) => line.split(','))
-        .map((student) => student.map((property) => property.trim()));
-      const studentsFields = new Map();
-      console.log(`Number of students: ${studentsArray.length}`);
-      for (const student of studentsArray) {
-        if (!studentsFields.has(student[3])) {
-          studentsFields.set(student[3], [student[0]]);
-        } else {
-          studentsFields.get(student[3]).push(student[0]);
-        }
+  const file = fsPromises.readFile(aPath, { encoding: 'utf8' });
+  file.then((fileResponse) => {
+    const linesArray = fileResponse.split('\n');
+    const studentsArray = linesArray
+      .slice(1, -1).filter((line) => line.trim().length !== 0)
+      .map((line) => line.split(','))
+      .map((student) => student.map((property) => property.trim()));
+    const studentsFields = new Map();
+    console.log(`Number of students: ${studentsArray.length}`);
+    for (const student of studentsArray) {
+      if (!studentsFields.has(student[3])) {
+        studentsFields.set(student[3], [student[0]]);
+      } else {
+        studentsFields.get(student[3]).push(student[0]);
       }
-      for (const [field, students] of studentsFields.entries()) {
-        console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
-      }
-    });
-    return file;
-  } catch (err) {
+    }
+    for (const [field, students] of studentsFields.entries()) {
+      console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
+    }
+  }).catch(() => {
     throw new Error('Cannot load the database');
-  }
+  });
+  return file;
 }
 
 module.exports = countStudents;
