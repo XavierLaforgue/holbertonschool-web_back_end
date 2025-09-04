f3862206119f8a5f495b3bfe9dea133ab01bbef1
XavierLaforgue <xavierjimmys@gmail.com>
2025-09-04 19:10:10 +0200

    full_server/ create
    Create a complex http server using express

---
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,27 @@
+import readDatabase from '../utils';
+
+export default class StudentsController {
+  static getAllStudents(request, response) {
+    const output = ['This is the list of our students'];
+    readDatabase(process.argv[2])
+      .then((readData) => {
+        for (const field of Object.keys(readData).sort()) {
+          output.push(`Number of students in ${field}: ${readData[field].length}. List: ${readData[field].join(', ')}`);
+        }
+        response.status(200).send(output.join('\n'));
+      })
+      .catch((err) => response.status(500).send(err));
+  }
+
+  static getAllStudentsByMajor(request, response) {
+    if (!['CS', 'SWE'].includes(request.params.major)) {
+      response.status(500).send('Major parameter must be CS or SWE');
+    } else {
+      readDatabase(process.argv[2])
+        .then((readData) => {
+          response.status(200).send(`List: ${readData[request.params.major].join(', ')}`);
+        })
+        .catch((err) => response.status(500).send(err));
+    }
+  }
+}
