ef7c716a50f0adfabd36a74dacd563d9da3d2748
XavierLaforgue <xavierjimmys@gmail.com>
2025-09-05 15:08:31 +0200

    utils.js update
    return a promise rejection instead of throwing the error

--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -2,22 +2,22 @@ import fs from 'node:fs/promises';
 
 export default function readDatabase(path) {
   const database = fs.readFile(path, { encoding: 'utf8' });
-  return database.then((readData) => {
-    const linesArray = readData.split('\n');
-    const studentsArray = linesArray
-      .slice(1, -1).filter((line) => line.trim().length !== 0)
-      .map((line) => line.split(','))
-      .map((student) => student.map((property) => property.trim()));
-    const studentsFields = {};
-    for (const student of studentsArray) {
-      if (!(student[3] in studentsFields)) {
-        studentsFields[student[3]] = [student[0]];
-      } else {
-        studentsFields[student[3]].push(student[0]);
+  return database
+    .then((readData) => {
+      const linesArray = readData.split('\n');
+      const studentsArray = linesArray
+        .slice(1, -1).filter((line) => line.trim().length !== 0)
+        .map((line) => line.split(','))
+        .map((student) => student.map((property) => property.trim()));
+      const studentsFields = {};
+      for (const student of studentsArray) {
+        if (!(student[3] in studentsFields)) {
+          studentsFields[student[3]] = [student[0]];
+        } else {
+          studentsFields[student[3]].push(student[0]);
+        }
       }
-    }
-    return studentsFields;
-  }).catch(() => {
-    throw new Error('Cannot load the database');
-  });
+      return studentsFields;
+    })
+    .catch(() => Promise.reject(new Error('Cannot load the database')));
 }
